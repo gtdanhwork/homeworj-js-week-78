4190cf512bf512317d3cac8727fe2f3b135498e8
ThanhDanh <gtdanh.work@gmail.com>
2026-08-01 22:55:12 +0700

    Add Sum Function

--- src/js/array.js
+++ src/js/array.js
@@ -2,14 +2,21 @@ let curArray = [];
 
 function addValue() {
 	let value = Number(document.getElementById('numberInput').value);
-	console.log(value);
 	curArray.push(value);
 	document.querySelector('#curArrayInfo').innerHTML = curArray;
 }
 
 document.querySelector('#btnInput').addEventListener('click', addValue);
 
-function sumArray() {}
+function sumArray() {
+	let value = 0;
+	for (let index = 0; index < curArray.length; index++) {
+		if (curArray[index] > 0) value += curArray[index];
+	}
+	document.querySelector('#btnSumResult').innerHTML = value;
+}
+
+document.querySelector('#btnSum').addEventListener('click', sumArray);
 
 function displayArray() {
 	document.getElementById('');
