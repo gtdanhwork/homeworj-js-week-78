c43b6376a487394b11455d97e3abc251ff353cb2
ThanhDanh <gtdanh.work@gmail.com>
2026-08-01 22:25:38 +0700

    Add Layout

---
+++ src/js/array.js
@@ -0,0 +1,18 @@
+
+let curArray = [];
+
+function addValue() {
+    let value = Number(document.getElementById("inputNumber").value);
+    curArray.push(value);
+}
+
+
+
+function sumArray() {
+    
+}
+
+
+function displayArray(){
+    document.getElementById("")
+}
