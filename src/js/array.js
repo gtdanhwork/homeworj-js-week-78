9333311e406882baf4d77b236a6896f18a27ef5a
ThanhDanh <gtdanh.work@gmail.com>
2026-08-01 22:30:16 +0700

    Add Value to Array Function

--- src/js/array.js
+++ src/js/array.js
@@ -1,18 +1,16 @@
-
 let curArray = [];
 
 function addValue() {
-    let value = Number(document.getElementById("inputNumber").value);
-    curArray.push(value);
+	let value = Number(document.getElementById('numberInput').value);
+	console.log(value);
+	curArray.push(value);
+	document.querySelector('#curArrayInfo').innerHTML = curArray;
 }
 
+document.querySelector('#btnInput').addEventListener('click', addValue);
 
+function sumArray() {}
 
-function sumArray() {
-    
-}
-
-
-function displayArray(){
-    document.getElementById("")
+function displayArray() {
+	document.getElementById('');
 }
