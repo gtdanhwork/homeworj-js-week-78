d9ff7f0120908a2f50b8944fa1519f9715aa2f0e
ThanhDanh <gtdanh.work@gmail.com>
2026-08-02 22:40:43 +0700

    Add Find Smallest Function

--- src/js/array.js
+++ src/js/array.js
@@ -18,6 +18,39 @@ function sumArray() {
 
 document.querySelector('#btnSum').addEventListener('click', sumArray);
 
-function displayArray() {
-	document.getElementById('');
+function countArray() {
+	let value = 0;
+	for (let index = 0; index < curArray.length; index++) {
+		if (curArray[index] > 0) {
+			value++;
+		}
+	}
+	document.querySelector('#btnCountResult').innerHTML = value;
 }
+
+document.querySelector('#btnCount').addEventListener('click', countArray);
+
+function findSmallestNumber() {
+	let value = curArray[0];
+	for (let index = 1; index < curArray.length; index++) {
+		if (curArray[index] < value) value = curArray[index];
+	}
+	document.querySelector('#btnSmallestResult').innerHTML = value;
+}
+
+document
+	.querySelector('#btnSmallest')
+	.addEventListener('click', findSmallestNumber);
+
+function findSmallestPositiveNumber() {
+	let value = curArray[0];
+	for (let index = 1; index < curArray.length; index++) {
+		if (curArray[index] < value && curArray[index] > 0)
+			value = curArray[index];
+	}
+	document.querySelector('#btnSmallestPositiveResult').innerHTML = value;
+}
+
+document
+	.querySelector('#btnSmallest')
+	.addEventListener('click', findSmallestPositiveNumber);
